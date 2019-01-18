e5b8c2f9f9293a80bea3ffd9b13a8f8cba8331da
Salman Qamar Khan <work.salmanqamarkhan@gmail.com>
2019-01-18 21:48:40 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'farmside');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'farmsideuser');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'salman');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
